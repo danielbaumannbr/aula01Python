fc5b4e95baed352e711d4215c1768299d25b4d4b
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2025-09-05 15:57:13 -0300

    Update script to greet user by name
    Replaces number input and sum logic with a prompt for the user's name and a personalized greeting using f-string formatting.

--- leia.py
+++ leia.py
@@ -1,4 +1,3 @@
-numero1 = input("Informe um número")
-numero2 = input("Informe outro número")
-soma=numero1+numero2
-print(" A soma é ",soma)
+nome=input("Qual seu nome?")
+#print("Boa tarde",nome)
+print(f"Boa tarde {nome}")
