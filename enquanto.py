ef990ff8ccbc13358557d2cca3f3793a2407f8cb
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2025-09-05 17:24:03 -0300

    Add countdown and food preference example scripts
    Added 'comando para.py' and 'enquanto.py' scripts demonstrating countdowns using for and while loops with time delays. Also added 'outro exemplo para.py' to print food preferences from a list.

---
+++ enquanto.py
@@ -0,0 +1,10 @@
+import time
+import os
+segundos = 10
+print("Iniciando a contagem")
+while segundos>-1:
+    print(segundos)
+    segundos=segundos-1
+    time.sleep(1)
+    
+print("Fim da contagem")
---
+++ outro exemplo para.py
@@ -0,0 +1,3 @@
+comidas = ["pizza","cachorro quente","hambuger"]
+for comida in comidas:
+    print("Eu gosto de",comida)
